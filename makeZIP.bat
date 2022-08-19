f4609aeae6775f6b56e0e814865d0b8324d08cf5
Archer36 <archer36@gmail.com>
2022-08-19 00:13:26 -0400

    Modified build script for my system

--- makeZIP.bat
+++ makeZIP.bat
@@ -9,42 +9,63 @@ rem #############################################
 rem  Remove ealier builds and create a new one
 rem  #########################################
 
+echo [*] Capture current directory
+set cur_dir=%cd%
+
+echo [*] Removing any old build/update directory
 Rmdir /S /Q _build_update_
+echo [*] Make the new build/update directory
 Mkdir _build_update_
+echo [*] CD to the new build/update directory
 Cd _build_update_
 
-git clone --branch master https://github.com/tomvandeneede/p2pp.git
+echo [*] Clone the repo
+git clone --branch zero-width-extrusion https://github.com/Archer36/p2pp.git
 
 rem Create the new BUILD
 rem ####################
+echo [*] CD to the cloned repo
 cd p2pp
+echo [*] Running the setup.py file with argument build
 python setup.py build
 
 rem Determine the version
 rem ####################
+echo [*] Get the P2PP Version
 python version.py >out.txt
+echo [*] Set version env var
 set /p version=<out.txt
+echo [*] Delete the version out.txt file
 del out.txt
 
+echo [*] Set the name env var
 set name=p2pp_%version%
 
+echo [*] CD to build directory
 cd build
+
+echo [*] Removing any old versions of the p2pp directory
 Rmdir /S /Q p2pp
 
 rem create a ZIP file
 rem #################
-move exe.win-amd64-3.9 p2pp
+echo [*] Renaming cxfreeze directory to p2pp
+move exe.win-amd64-3.10 p2pp
+echo [*] Deleting any old versions of the zip files.
 del %name%.zip
-"c:\Program Files\WinRAR\WinRar.exe" a -m5 -afzip -y %name%.zip p2pp
+echo [*] Creating Zip
+"c:\Program Files\7-Zip\7z.exe" a -tzip %name%.zip p2pp
 
 rem copy the file to dropbox
 rem ########################
-copy %name%.zip "c:\users\tomvandeneede\dropbox\public\p2pp\Development\Windows\"
+echo [*] Copy the zip file to the downloads directory
+copy %name%.zip %USERPROFILE%\Downloads
 
 rem # go up to the top level #
-cd ..
-cd ..
-cd ..
+cd %cur_dir%
+echo [*] Removing the build/update directory
 Rmdir /S /Q _build_update_
+
+echo [*] Done!
 
 
